ea2ea59fafae7bd7d433de1319edc227aa7f45a2
채주완 <81293158+joowan1108@users.noreply.github.com>
2024-10-12 22:37:28 +0900

    [Silver I] Title: 미로 탐색, Time: 60 ms, Memory: 34068 KB -BaekjoonHub

--- Python/백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ Python/백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,27 +1,32 @@
 import sys
-from collections import deque
-n,m = map(int, sys.stdin.readline().rstrip().split())
+from  collections import deque
+
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
 
 graph = []
-moves = [(0,1),(0,-1),(1,0),(-1,0)]
-
 for _ in range(n):
-    graph.append(list(map(int,sys.stdin.readline().rstrip())))
+	graph.append(list(map(int, input().strip())))
 
-def bfs(start_h, start_w):
-    need_visited = deque()
-    need_visited.append((start_h, start_w))
-    while need_visited:
-        h,w= need_visited.popleft()
-        for dh, dw in moves:
-            new_h = h+dh
-            new_w = w+dw
-            if(0<=new_h<n and 0<=new_w<m and graph[new_h][new_w]==1):
-                need_visited.append((new_h, new_w))
-                graph[new_h][new_w] = graph[h][w] + 1
-    
-    
-bfs(0,0)
-print(graph[n-1][m-1])
+moves = [(1,0),(-1,0),(0,1),(0,-1)]
 
-    
+def bfs(x_s,y_s):
+	q = deque()
+	q.append((x_s,y_s))
+	while q:
+		x,y = q.popleft()
+		for move in moves:
+			xx = x+move[0]
+			yy = y+move[1]
+			if(0<=xx<n and 0<=yy<m and graph[xx][yy]==1):
+				graph[xx][yy] = graph[x][y]+1
+				q.append((xx,yy))
+	return graph[n-1][m-1]
+
+result = bfs(0,0)
+print(result)
+		
+
+
+	
