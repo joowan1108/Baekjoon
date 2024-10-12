18be91c1def147f053991e4e723446249203fef3
채주완 <81293158+joowan1108@users.noreply.github.com>
2024-10-12 20:40:23 +0900

    [Silver I] Title: 회의실 배정, Time: 216 ms, Memory: 52684 KB -BaekjoonHub

--- Python/백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ Python/백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,15 +1,21 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
+
 times = []
 for _ in range(n):
-    s, e = map(int, input().split())
-    times.append((s,e))
-times = sorted(times, key = lambda x:(x[1], x[0]))
-cnt = 1
-idx=0
-for i in range(idx+1, n):
-    if(times[idx][1] <= times[i][0]):
-        cnt+=1
-        idx=i
-    
+	a,b = map(int, input().split())
+	times.append((a,b))
+
+times = sorted(times, key=lambda x: (x[1], x[0]))
+start = 0
+end = 0
+cnt = 0
+for x,y in times:
+	if(x>=end):
+		cnt+=1
+		end = y
+
 print(cnt)
-    
+	
