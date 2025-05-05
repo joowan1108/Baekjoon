baf546e22ec698f004b78b73ab875f6eedae5cbb
채주완 <81293158+joowan1108@users.noreply.github.com>
2025-05-05 20:15:29 +0900

    [level 3] Title: 네트워크, Time: 0.77 ms, Memory: 9.32 MB -BaekjoonHub

--- Python3/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python3/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,37 +1,17 @@
-def find(parents, comp):
-    if parents[comp]!=comp:
-        parents[comp] = find(parents, parents[comp])
-    return parents[comp]
-    
-
-def union(parents, rank, comp1, comp2):
-    root1 = find(parents, comp1)
-    root2 = find(parents, comp2)
-    
-    if root1 != root2:
-        if rank[root1]<rank[root2]:
-            parents[root1] = root2
-        elif rank[root1] == rank[root2]:
-            parents[root1] = root2
-            rank[root2]+=1
-        else:
-            parents[root2] = root1
-    
+def dfs(computers, visited, node):
+    visited[node]=True
+    for i,connected in enumerate(computers[node]):
+        if not visited[i] and connected==1:
+            dfs(computers, visited, i)
+        
 
 def solution(n, computers):
-    parents = [i for i in range(n)]
-    rank = [0]*n
+    visited = [False]*n
+    ans = 0
     for i in range(n):
-        for j in range(n):
-            if computers[i][j]==1:
-                union(parents, rank, i, j)
-    
-    for i in range(len(parents)):
-        parents[i] = find(parents, i)
-    network = set(parents)
-    return len(network)
-    
-    
-
-    
+        if not visited[i]:
+            dfs(computers, visited, i)
+            ans+=1
+    return ans
+        
     
