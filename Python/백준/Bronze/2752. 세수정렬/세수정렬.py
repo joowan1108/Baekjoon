7437057c417422903ae6cd180cc7470f389d7699
채주완 <81293158+joowan1108@users.noreply.github.com>
2025-08-18 20:46:00 +0900

    [Bronze IV] Title: 세수정렬, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2752. 세수정렬/세수정렬.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+nums = list(map(int, input().split()))
+nums.sort()
+for num in nums:
+  print(num)
+    
+    
+    
+
+  
+
+    
+    
+    
+
+
+
+
+  
+    
