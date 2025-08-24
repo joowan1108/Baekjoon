3b549e574409d1da4211978ba3a69c705425535e
채주완 <81293158+joowan1108@users.noreply.github.com>
2025-08-24 14:55:12 +0900

    [Gold IV] Title: 동전 1, Time: 256 ms, Memory: 33432 KB -BaekjoonHub

--- Python/백준/Gold/2293. 동전 1/동전 1.py
+++ Python/백준/Gold/2293. 동전 1/동전 1.py
@@ -3,6 +3,9 @@ input = sys.stdin.readline
 
 """
 dp[i]에는 가치 i를 이루는 가짓수를 저장함
+이때 가치 i에서부터 모든 coin들을 더해서 가짓수를 update하면 중복이 생긴다.
+
+각 index에서 쓸 수 있는 코인을 하나로 제한해서 풀면 중복이 안된다.
 """
 
 n,k = map(int, input().split())
