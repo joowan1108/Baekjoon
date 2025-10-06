43a846bc638c11f37e901fd65731528b1e19ed98
채주완 <81293158+joowan1108@users.noreply.github.com>
2025-10-06 14:32:20 +0900

    [Gold III] Title: 웜홀, Time: 1016 ms, Memory: 33432 KB -BaekjoonHub

--- Python/백준/Gold/1865. 웜홀/웜홀.py
+++ Python/백준/Gold/1865. 웜홀/웜홀.py
@@ -1,33 +1,50 @@
 import sys
 input = sys.stdin.readline
+INF = 1000000000
 
-TC = int(input())
-for _ in range(TC):
-  n,m,w = map(int, input().split())
-  graph = [[] for _ in range(n+1)]
-  for _ in range(m):
-    s,e,t = map(int, input().split())
-    graph[s].append((e,t))
-    graph[e].append((s,t))
-    
-  for _ in range(w):
-    s,e,t = map(int, input().split())
-    graph[s].append((e,-t))
 
-  
-  distances = [int(1e9)] * (n+1)
-  cycle = False
+def bellmanFord():
+    global isAvailable
 
-  for checks in range(n+1):
-    for mid in range(1,n+1):
-      for end,cost in graph[mid]:
-        if distances[end] > distances[mid] + cost:
-          distances[end] = distances[mid] + cost
-          # n번째 check에도 이 경우가 발생한다면 음의 순환 존재
-          if checks == n:
-            cycle = True
-  if cycle:
-    print("YES")
-  else:
-    print("NO")
-            
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            for target, wasted in info[j]:
+                if dp[target] > wasted + dp[j]:
+                    dp[target] = wasted + dp[j]
+
+                    if i == n:
+                        isAvailable = True
+
+
+if __name__ == '__main__':
+    # 입력
+    tc = int(input())
+
+    for _ in range(tc):
+        n, m, w = map(int, input().split())
+
+        info = [[] for _ in range(n+1)]
+
+        # 도로
+        for _ in range(m):
+            s, e, t = map(int, input().split())
+
+            info[s].append((e, t))
+            info[e].append((s, t))
+
+        # 웜홀
+        for _ in range(w):
+            s, e, t = map(int, input().split())
+
+            info[s].append((e, -t))
+
+        # 탐색
+        isAvailable = False
+        dp = [INF]*(n+1)
+        bellmanFord()
+
+        # 출력
+        if isAvailable:
+            print("YES")
+        else:
+            print("NO")
