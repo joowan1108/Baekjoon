b6b0b539fc606259b9e2c4299cf22b673e001a0b
채주완 <81293158+joowan1108@users.noreply.github.com>
2025-10-09 19:28:53 +0900

    [Gold IV] Title: 부분합, Time: 128 ms, Memory: 42168 KB -BaekjoonHub

--- Python/백준/Gold/1806. 부분합/부분합.py
+++ Python/백준/Gold/1806. 부분합/부분합.py
@@ -1,56 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-
-"""
-
-prefix sum으로도 모든 길이에 대한 경우의 수를 구하려면 N^2이 걸림
--> 최소 길이 iteration을 binary search로 한다면 NlogN으로 구할 수 있음
-"""
-
 n,s = map(int, input().split())
 nums = [0] + list(map(int, input().split()))
-prefix_sum = [0]*(n+1)
 
+
+prefix_sum = [0]*(n+1)
 for i in range(1,n+1):
   prefix_sum[i] = prefix_sum[i-1] + nums[i]
 
-start = 1
-end = n
+start, end = 0,0
 ans = int(1e9)
-while start <= end:
-  mid = (start+end)//2
-  found = False
-  for j in range(n+1-mid):
-    sum_ = prefix_sum[j+mid] - prefix_sum[j]
-    if sum_ >= s:
-      found = True
-      break
-  #mid 길이로 s가 된다면, 더 짧은 길이 시도
-  if found:
-    end = mid-1
-    ans = min(ans, mid)
+while start <= end and end<=n:
+  if prefix_sum[end] - prefix_sum[start] < s:
+    end += 1
   else:
-    start = mid+1
+    ans = min(ans,end - start)
+    start += 1
 
 if ans == int(1e9):
   print(0)
 else:
   print(ans)
-      
-      
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+  
